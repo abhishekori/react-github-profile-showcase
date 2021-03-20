ecb99471a3a5f4fb8081e0484a41289637e6ed7a
abhishekori <abhikori1994@gmail.com>
2021-03-20 20:16:36 +0530

    Updates

---
+++ precache-manifest.d1a308a44a884b0dc2680310087710a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942cae0df20ac2f73f6f772924ba4093",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45232afe4af4690273c0",
+    "url": "./static/css/2.02adba32.chunk.css"
+  },
+  {
+    "revision": "768aa60772328cbba48c",
+    "url": "./static/css/main.c260cd8d.chunk.css"
+  },
+  {
+    "revision": "45232afe4af4690273c0",
+    "url": "./static/js/2.307004f7.chunk.js"
+  },
+  {
+    "revision": "6053e451d7862a9dc5b0526c27e6d0db",
+    "url": "./static/js/2.307004f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768aa60772328cbba48c",
+    "url": "./static/js/main.1370ba09.chunk.js"
+  },
+  {
+    "revision": "c6d3b7371c24c21bd1d8",
+    "url": "./static/js/runtime-main.b844f6dd.js"
+  }
+]);
